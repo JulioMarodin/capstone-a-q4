675cfb9a18a83934577f23593e4309ba0bbb6832
Julio Marodin <juliocesar_cocacola@hotmail.com>
2022-04-12 15:01:54 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import app from "./app";
+import dbConfig from "./db/ormconfig";
+
+createConnection(dbConfig).then(() => {
+  const PORT = process.env.PORT ?? 3000;
+  console.log("Database connected");
+  app.listen(() => 
+    console.log(`App is running on http://localhost:${PORT}`)
+  );
+}).catch((err) => console.log(err));
