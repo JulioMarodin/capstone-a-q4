43c2f07dff3370ad640e5df259576115107280b6
IqueMoraes <iquedemoraes@hotmail.com>
2022-04-14 11:03:58 -0300

    feat: inclued authors entity class

---
+++ src/entities/Authors.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity("authors")
+export class Authors {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false, unique: true, length: 128 })
+  name: string;
+
+  @Column({ length: 128 })
+  country: string;
+
+  @Column()
+  birthday: Date;
+
+  @Column()
+  death_date: Date;
+}
