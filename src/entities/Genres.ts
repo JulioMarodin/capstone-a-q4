27a42712f90bd7de156119005336b071452f7aa3
IqueMoraes <iquedemoraes@hotmail.com>
2022-04-14 12:22:40 -0300

    feat: inclued genres entity class

---
+++ src/entities/Genres.ts
@@ -0,0 +1,12 @@
+import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+
+@Entity('genres')
+export class Genres {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ nullable: false, unique:true ,length: 128 })
+    name: string;
+
+}
