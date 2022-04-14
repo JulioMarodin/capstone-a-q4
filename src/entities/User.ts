30615904275bf343bf5514fc5c4933fff5dc7ddc
rafaelgsousa <elderrafaelgomes@gmail.com>
2022-04-14 17:01:45 -0300

    refactor(entities):correction in the typing of the id column of the users entity

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -4,7 +4,7 @@ import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
 export class Users {
 
     @PrimaryGeneratedColumn('uuid')
-    id?: number;
+    id?: string;
 
     @Column({ length: 128 })
     name: string;
