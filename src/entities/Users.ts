28e2974bad9e8b5b26147f45f3a189fc62909072
rafaelgsousa <elderrafaelgomes@gmail.com>
2022-04-18 09:05:32 -0300

    feat(repositories/Books): creation of booksRepository

---
+++ src/entities/Users.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Users {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ length: 128 })
+    name: string;
+
+    @Column({ length: 128 })
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    biography: string;
+
+    @Column()
+    birthday: Date;
+
+    @Column({ length: 128 })
+    city: string;
+}
