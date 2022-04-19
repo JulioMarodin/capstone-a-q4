8c5c1dc58c9f14e56f8058c6969d006063b7f7c2
Julio Marodin <juliocesar_cocacola@hotmail.com>
2022-04-19 16:02:35 -0300

    feat: repository postlikes

---
+++ src/repositories/PostLikes/index.ts
@@ -0,0 +1,23 @@
+import { getRepository, Repository } from "typeorm";
+import { PostLike } from "../../entities/PostLike";
+import { IdataUpdatePostLikes, IPostLikesRepo } from "./interfaces";
+
+class postsLikesRepository implements IPostLikesRepo {
+  private ormPostsLikesRepo : Repository<PostLike>;
+
+  constructor() {
+    this.ormPostsLikesRepo = getRepository(PostLike);
+  }
+
+  createLike = (like: PostLike) => this.ormPostsLikesRepo.create(like);
+
+  saveLike = async (like: PostLike) => await this.ormPostsLikesRepo.save(like);
+
+  findLikes = async () => await this.ormPostsLikesRepo.find();
+
+  updateLike = async (dataLike: IdataUpdatePostLikes, update: IdataUpdatePostLikes) => await this.ormPostsLikesRepo.update(dataLike, update);
+
+  deleteLike = async (dataLike: IdataUpdatePostLikes) => await this.ormPostsLikesRepo.delete(dataLike);
+}
+
+export default postsLikesRepository;
