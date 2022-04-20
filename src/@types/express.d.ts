c2b5238ffb695c30a2326d6b55d94d7c258e2581
rafaelgsousa <elderrafaelgomes@gmail.com>
2022-04-20 08:28:49 -0300

    refactor(controllers/authors): parameter typing

--- src/@types/express.d.ts
+++
@@ -1,9 +0,0 @@
-import { IAuthor } from '../repositories/Authors/interfaces';
-
-declare global{
-    namespace Express {
-        interface Request {
-            params: number;
-        }
-    }
-}
