f4c3c9b4f970632d5fb49d8230fb6960dddd70b5
Julio Marodin <juliocesar_cocacola@hotmail.com>
2022-04-20 16:52:00 -0300

    feat: edit ormconfig ts

--- src/db/ormconfig.ts
+++ src/db/ormconfig.ts
@@ -8,8 +8,8 @@ const prodConfig = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
   logging: false,
-  entities: [path.join(__dirname, '../entities/**/*.ts')],
-  migrations: [path.join(__dirname, '../migrations/**/*.ts')],
+  entities: [path.join(__dirname, '../entities/**/*.*')],
+  migrations: [path.join(__dirname, '../migrations/**/*.*')],
   cli: {
     entitiesDir: path.join(__dirname, '../entities'),
     migrationsDir: path.join(__dirname, '../migrations'),
