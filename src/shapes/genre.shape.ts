70545e3011dab304b468e1165868d891fa44d897
rafaelgsousa <elderrafaelgomes@gmail.com>
2022-04-21 00:46:43 -0300

    feat(routes/Genre):create createGenre route

--- src/shapes/genre.shape.ts
+++ src/shapes/genre.shape.ts
@@ -1,8 +1,7 @@
 import * as yup from 'yup';
-import { transformToTitle } from '../utils';
 
 const genreShape = yup.object().shape({
-    name: yup.string().required().transform((name) => transformToTitle(name)),
+    name: yup.string().required(),
 });
 
 export default genreShape;
