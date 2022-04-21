1f7f4703fe8f4423aaf21a2b45c7e5d40711e921
Ricardo Oliveira <ricardonegocios700@gmail.com>
2022-04-21 14:40:44 -0300

    feat(route/genre-delete): include in route

--- src/routes/Genres/genres.routes.ts
+++ src/routes/Genres/genres.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { createGenreController } from '../../controllers/Genres';
+import { deleteGenreController, createGenreController } from '../../controllers/Genres';
 import { validateShape, verifyAuth, isAdmin } from '../../middlewares';
 import { genreShape } from '../../shapes';
 
 const routesGenre = Router();
 
 routesGenre.post('', validateShape(genreShape), verifyAuth, isAdmin, createGenreController);
+routesGenre.delete('/name', verifyAuth, isAdmin, deleteGenreController);
 
 export default routesGenre;
