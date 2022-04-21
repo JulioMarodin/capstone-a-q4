a221a1f2dceda6280cb76bc5c8b424c4e780c5b5
rafaelgsousa <elderrafaelgomes@gmail.com>
2022-04-21 13:10:47 -0300

    feat(routes/Genres):create getGenre route

--- src/controllers/Genres/index.ts
+++ src/controllers/Genres/index.ts
@@ -1,3 +1,4 @@
 import createGenreController from './createGenre.controller';
+import getGenre from './getGenre.controller';
 
-export { createGenreController };
+export { createGenreController, getGenre };
--- src/routes/Genres/genres.routes.ts
+++ src/routes/Genres/genres.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { createGenreController } from '../../controllers/Genres';
+import { createGenreController, getGenre } from '../../controllers/Genres';
 import { validateShape, verifyAuth, isAdmin } from '../../middlewares';
 import { genreShape } from '../../shapes';
 
 const routesGenre = Router();
 
 routesGenre.post('', validateShape(genreShape), verifyAuth, isAdmin, createGenreController);
+
+routesGenre.get('/:id', getGenre);
 
 export default routesGenre;
