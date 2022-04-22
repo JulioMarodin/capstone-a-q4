794fde9e70da5d206f735a3da8c5b2f92a4094f8
IqueMoraes <iquedemoraes@hotmail.com>
2022-04-22 19:56:03 -0300

    fix: fixed url access

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import "reflect-metadata";
-import { createConnection } from "typeorm";
-import app from "./app";
-import dbConfig from "./db/ormconfig";
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import app from './app';
+import dbConfig from './db/ormconfig';
 
 createConnection(dbConfig).then(() => {
   const PORT = process.env.PORT ?? 3000;
-  console.log("Database connected");
+  console.log('Database connected');
   app.listen(process.env.PORT || 3000);
 }).catch((err) => console.log(err));
