a1d9ad23bec8c6bab8dd855523803c6d470bb262
Ricardo Oliveira <ricardonegocios700@gmail.com>
2022-04-25 14:22:30 -0300

    feat(route/post-delete): create

--- src/routes/Posts/posts.routes.ts
+++ src/routes/Posts/posts.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getPosts, getPostsByAuthor } from '../../controllers/Posts';
+import { deletePost, getPosts, getPostsByAuthor } from '../../controllers/Posts';
 import { verifyAuth } from '../../middlewares';
 
 const routesPosts = Router();
@@ -7,5 +7,7 @@ const routesPosts = Router();
 routesPosts.get('', verifyAuth, getPosts);
 
 routesPosts.get('/:author_id', verifyAuth, getPostsByAuthor);
+
+routesPosts.delete('/:id', verifyAuth, deletePost);
 
 export default routesPosts;
