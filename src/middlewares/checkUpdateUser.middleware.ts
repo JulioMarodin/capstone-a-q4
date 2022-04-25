3b48eac7b27ec1f524dd6bb7284756970e2aaf33
rafaelgsousa <elderrafaelgomes@gmail.com>
2022-04-25 17:28:57 -0300

    refactor(route/updateUser && utils): refactor updateUser && create utils

--- src/middlewares/checkUpdateUser.middleware.ts
+++ src/middlewares/checkUpdateUser.middleware.ts
@@ -1,16 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
 import { ErrorHandler } from '../services/errors';
+import { checkEmail } from '../utils';
 
 // Verifica se no corpo da requisição pretende-se editar a coluna admin, se houve então verifica se
 //  quem está tentando editar é um isAdmin.
-const checkUpdateUser = (req:Request, _res:Response, next: NextFunction) => {
+const checkUpdateUser = async (req:Request, _res:Response, next: NextFunction) => {
     try {
         if (Object.keys(req.body).filter((e) => e === 'admin').length === 1) {
             if (req.user.admin) {
+                checkEmail(req.body);
                 return next();
             }
             throw new ErrorHandler(401, 'Only admin can update admin status');
         }
+        checkEmail(req.body);
         return next();
     } catch (error) {
         return next(error);
