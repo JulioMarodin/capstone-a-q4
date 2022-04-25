301c8e9bde408d9db98ab2308aa313159504af4b
Ricardo Oliveira <ricardonegocios700@gmail.com>
2022-04-25 20:40:30 -0300

    refactory(middlewares/isAdminOrCreator): rename

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,7 +2,7 @@ import validateShape from './validateShape.middleware';
 import verifyAuth from './verifyAuth';
 import isAdmin from './isAdmin.middleware';
 import checkUnique from './checkUniqueValue';
-import checkMySelfMiddleware from './checkMySelf.middleware';
+import isAdminOrCreator from './isAdminOrCreator.middleware';
 import checkUniqueTitleAndIsbnBook from './checkUniqueTitleAndIsbnBook.middleware';
 import checkUpdateUser from './checkUpdateUser.middleware';
 
@@ -11,7 +11,7 @@ export {
  verifyAuth,
  isAdmin,
  checkUnique,
- checkMySelfMiddleware,
+ isAdminOrCreator,
  checkUniqueTitleAndIsbnBook,
  checkUpdateUser,
 };
