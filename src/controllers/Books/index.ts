5cee3edc9e2ce70ceae16f57ae4b2c6a22156afe
IqueMoraes <iquedemoraes@hotmail.com>
2022-04-25 09:06:48 -0300

    feat: created read one book route

--- src/controllers/Books/index.ts
+++ src/controllers/Books/index.ts
@@ -1,5 +1,11 @@
 import deleteBookController from './deleteBook.controller';
 import createBookController from './createBook.controller';
-import getBooksController from './getBook.controller';
+import getBooksController from './getBooks.controller';
+import getBookController from './getBook.controller';
 
-export { deleteBookController, createBookController, getBooksController };
+export {
+  deleteBookController,
+  createBookController,
+  getBooksController,
+  getBookController,
+};
--- src/routes/Books/books.routes.ts
+++ src/routes/Books/books.routes.ts
@@ -9,11 +9,16 @@ import {
   getBooksController,
 } from '../../controllers/Books';
 import { bookShape } from '../../shapes';
+import getBookController from '../../controllers/Books/getBook.controller';
 
 const routesBooks = Router();
 
 routesBooks.post('', verifyAuth, validateShape(bookShape), checkUniqueTitleAndIsbnBook, createBookController);
-routesBooks.delete('/:id', verifyAuth, isAdmin, deleteBookController);
+
 routesBooks.get('', verifyAuth, getBooksController);
+
+routesBooks.get('/:id', verifyAuth, getBookController);
+
+routesBooks.delete('/:id', verifyAuth, isAdmin, deleteBookController);
 
 export default routesBooks;
