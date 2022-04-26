d330711b6d93471e33043cda20c021fc52808a5f
Christian <christian.mf22@gmail.com>
2022-04-26 16:33:18 -0300

    feat: create relationalPosts object

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,16 +2,16 @@ import validateShape from './validateShape.middleware';
 import verifyAuth from './verifyAuth';
 import isAdmin from './isAdmin.middleware';
 import checkUnique from './checkUniqueValue';
-import isAdminOrCreator from './isAdminOrCreator.middleware';
+import checkMySelfMiddleware from './isAdminOrCreator.middleware';
 import checkUniqueTitleAndIsbnBook from './checkUniqueTitleAndIsbnBook.middleware';
 import checkUpdateUser from './checkUpdateUser.middleware';
 
 export {
- validateShape,
- verifyAuth,
- isAdmin,
- checkUnique,
- isAdminOrCreator,
- checkUniqueTitleAndIsbnBook,
- checkUpdateUser,
+  validateShape,
+  verifyAuth,
+  isAdmin,
+  checkUnique,
+  checkMySelfMiddleware,
+  checkUniqueTitleAndIsbnBook,
+  checkUpdateUser,
 };
--- src/routes/Users/users.routes.ts
+++ src/routes/Users/users.routes.ts
@@ -1,16 +1,8 @@
 import { Router } from 'express';
 
-import {
-  isAdmin, validateShape, verifyAuth, checkUpdateUser,
-} from '../../middlewares';
+import { isAdmin, validateShape, verifyAuth, checkUpdateUser } from '../../middlewares';
 import { userShape } from '../../shapes';
-import {
-  getUsers,
-  createUserController,
-  deleteUserController,
-  getOneUser,
-  updateUser,
-} from '../../controllers/Users';
+import { getUsers, createUserController, deleteUserController, getOneUser, updateUser } from '../../controllers/Users';
 
 const routesUsers = Router();
 
