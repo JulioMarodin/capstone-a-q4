5b8a4f44859539029e0ea04d3f20a7793fc11c69
rafaelgsousa <elderrafaelgomes@gmail.com>
2022-04-27 11:56:26 -0300

    refactor(shapes): refactor userBook and userBookUpdate

--- src/shapes/userBookUpdate.shape.ts
+++ src/shapes/userBookUpdate.shape.ts
@@ -5,7 +5,7 @@ const userBookUpdateShape = yup.object().shape({
   reading: yup.boolean().optional(),
   want_to_read: yup.boolean().optional(),
   favorites: yup.boolean().optional(),
-  rating: yup.number().optional(),
+  rating: yup.number().min(0).max(5).optional(),
   book_id: yup.number().optional(),
   user_id: yup.string().optional(),
 });
