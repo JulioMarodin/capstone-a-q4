b4406e693db75ce88d24421b11a5114bf5041c35
IqueMoraes <iquedemoraes@hotmail.com>
2022-04-28 19:05:59 -0300

    refactor: adjust middleware on create books

--- src/controllers/Books/createBook.controller.ts
+++ src/controllers/Books/createBook.controller.ts
@@ -1,9 +1,14 @@
 /* eslint-disable no-underscore-dangle */
 import { Request, Response } from 'express';
 import { BooksRepository } from '../../repositories';
+import { createAuthorIfNotExists, createPublisherIfNotExists } from '../../services';
 
 const createBookController = async (req: Request, res: Response) => {
   try {
+    createAuthorIfNotExists(req);
+    createPublisherIfNotExists(req);
+    req.validated.user = req.user;
+
     const book = new BooksRepository().createBook(req.validated);
     await new BooksRepository().saveBook(book);
     const bookToReturn = JSON.parse(JSON.stringify(book));
