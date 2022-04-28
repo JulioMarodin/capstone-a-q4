2fe620004396055cf0340bfeedf416b52513a515
Christian <christian.mf22@gmail.com>
2022-04-28 00:18:20 -0300

    fix: Created error handling for duplicity

--- src/controllers/Publishers/updatePublisher.controller.ts
+++ src/controllers/Publishers/updatePublisher.controller.ts
@@ -2,11 +2,17 @@ import { Request, Response } from 'express';
 import { PublisherRepository } from '../../repositories';
 import { ErrorHandler } from '../../services/errors';
 
-const updatePublisherController = async (req:Request, res:Response) => {
+const updatePublisherController = async (req: Request, res: Response) => {
   try {
+    const checkPublishers = await new PublisherRepository().findPublisherByName(req.body.name);
     const publisher = await new PublisherRepository().updatePublisher(req.params, req.validated);
+
     if (!publisher.affected) {
       throw new ErrorHandler(404, 'Publisher not found');
+    }
+
+    if (checkPublishers) {
+      throw new ErrorHandler(404, 'Publisher already exists');
     }
     return res.status(201).json(publisher);
   } catch (error) {
