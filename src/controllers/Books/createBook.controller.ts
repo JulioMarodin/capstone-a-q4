07a87dc908ea1e3f16f31db5b9e13eefb8dcadf3
Christian <christian.mf22@gmail.com>
2022-04-28 01:04:09 -0300

    fix: Created error handling for user not found

--- src/controllers/Books/createBook.controller.ts
+++ src/controllers/Books/createBook.controller.ts
@@ -1,10 +1,14 @@
+/* eslint-disable no-underscore-dangle */
 import { Request, Response } from 'express';
 import { BooksRepository } from '../../repositories';
 
-const createBookController = async (req:Request, res:Response) => {
-    const book = new BooksRepository().createBook(req.validated);
-    await new BooksRepository().saveBook(book);
-    return res.status(201).json(book);
+const createBookController = async (req: Request, res: Response) => {
+  const book = new BooksRepository().createBook(req.validated);
+  await new BooksRepository().saveBook(book);
+  const bookToReturn = JSON.parse(JSON.stringify(book));
+  delete bookToReturn.user;
+  delete bookToReturn.__author__;
+  return res.status(201).json(bookToReturn);
 };
 
 export default createBookController;
--- src/controllers/Books/getBook.controller.ts
+++ src/controllers/Books/getBook.controller.ts
@@ -1,9 +1,19 @@
 import { Request, Response } from 'express';
 import { BooksRepository } from '../../repositories';
+import { ErrorHandler } from '../../services/errors';
 
-const getBookController = async (req:Request, res:Response) => {
-  const book = await new BooksRepository().findBook(Number(req.params.id));
-  return res.status(200).json(book);
+const getBookController = async (req: Request, res: Response) => {
+  try {
+    const book = await new BooksRepository().findBook(Number(req.params.id));
+
+    if (!book) {
+      throw new ErrorHandler(404, 'Book not found');
+    }
+
+    return res.status(200).json(book);
+  } catch (e) {
+    return res.status(e.statusCode).json({ error: e.message });
+  }
 };
 
 export default getBookController;
