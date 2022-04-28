dca04296d30ce7729ac275409ca46d7e8c00d947
IqueMoraes <iquedemoraes@hotmail.com>
2022-04-28 19:43:02 -0300

    fix: limiting rating to 5 on shapes

--- src/shapes/userBook.shape.ts
+++ src/shapes/userBook.shape.ts
@@ -5,7 +5,7 @@ const userBookShape = yup.object().shape({
   reading: yup.boolean().default(false),
   want_to_read: yup.boolean().default(false),
   favorites: yup.boolean().default(false),
-  rating: yup.number().default(0),
+  rating: yup.number().default(0).max(5),
   book: yup.string().required(),
 });
 
--- src/shapes/userBookUpdate.shape.ts
+++ src/shapes/userBookUpdate.shape.ts
@@ -5,7 +5,7 @@ const userBookUpdateShape = yup.object().shape({
   reading: yup.boolean().optional(),
   want_to_read: yup.boolean().optional(),
   favorites: yup.boolean().optional(),
-  rating: yup.number().optional(),
+  rating: yup.number().optional().max(5),
   book: yup.string().optional(),
 });
 
